8fd844da25f8cb8c090c67dd663c1bb89373c114
evan999 <emeshberge@gmail.com>
2021-03-10 23:29:51 -0500

    Initialize react project.

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import Pusher from 'pusher'
+
+// app config
+const app = express();
+
+// middleware
+
+// DB config
+
+// API routes
+
+// listener
