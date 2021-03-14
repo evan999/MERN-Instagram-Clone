68b8c769b8275e5f880cb88c344daaee0bdd1fc1
evan999 <emeshberge@gmail.com>
2021-03-13 23:14:54 -0500

    Add in API routes.

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import Pusher from 'pusher'
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import Pusher from 'pusher';
 
 // app config
 const app = express();
+const port = process.env.PORT || 8080;
 
 // middleware
+app.use(express.json());
+app.use(cors());
 
 // DB config
 
 // API routes
+app.get('/', (req, res) => res.status(200).send('Hello world'));
+
 
 // listener
+app.listen(port, () => console.log(`Listening on localhost:${port}`));
+
+
